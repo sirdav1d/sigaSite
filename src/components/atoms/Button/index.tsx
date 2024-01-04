67c94195d00d8653d7b3299b3fba794e0f611b0e
sirdav1d <ddavid.diniz@gmail.com>
2024-01-04 04:11:56 -0300

    fix: :lipstick: ajuste de feedback de botões

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -13,7 +13,7 @@ export default function Button({ children, icon, role, href }: ButtonProps) {
 	return (
 		<div
 			data-role={role}
-			className='py-4 pl-6 pr-8 bg-gradient-to-r w-fit from-brand-blueSiga-500 bg-[length:300px_400px] bg-left data-[role=whatsapp]:to-[#008000] to-brand-orangeSiga-500 rounded active:shadow-lg shadow-sm hover:bg-right transition-all bg-no-repeat duration-300 cursor-pointer ease-linear hover:shadow-md text-brand-neutralSiga-100 font-title font-bold'>
+			className='py-4 pl-6 pr-8 bg-gradient-to-r w-fit from-brand-blueSiga-500 bg-[length:364px_400px] bg-left data-[role=whatsapp]:to-[#008000] to-brand-orangeSiga-500 rounded active:shadow-lg shadow-sm hover:bg-right transition-all bg-no-repeat duration-300 cursor-pointer ease-linear hover:shadow-md text-brand-neutralSiga-100 font-title font-bold'>
 			<a
 				data-role={role}
 				className='flex gap-3 items-center justify-center drop-shadow-sm data-[role=whatsapp]:text-base text-xl'
