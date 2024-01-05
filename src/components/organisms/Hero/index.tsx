6ef56db09d9c0daac271c29ec2cf58dadbbefb66
sirdav1d <ddavid.diniz@gmail.com>
2024-01-05 18:25:11 -0300

    fix: :lipstick: ajuste de bgHero

--- src/components/organisms/Hero/index.tsx
+++ src/components/organisms/Hero/index.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
 			<>
 				<div
 					id='hero'
-					className='absolute w-screen h-full bg-brand-neutralSiga-900/60 bg-blend-overlay bg-no-repeat bg-fill bg-center bg-fixed top-0 left-0 bg-bgHero -z-10'></div>
+					className='absolute w-screen h-full bg-brand-neutralSiga-900/60 bg-blend-overlay bg-no-repeat bg-cover bg-center lg:bg-fixed top-0 left-0 bg-bgHero -z-10'></div>
 				<div className='max-w-4xl h-full w-full mx-auto text-center flex flex-col gap-6 drop-shadow-md items-center justify-center'>
 					<Heading />
 					<p className='font-body font-light prose prose-sm lg:prose-xl mb-5 text-brand-neutralSiga-100'>
