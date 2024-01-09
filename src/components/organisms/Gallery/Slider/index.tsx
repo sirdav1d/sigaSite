7cf203aa5db06e1d1efcbb4c24ce39b8bef7066e
sirdav1d <ddavid.diniz@gmail.com>
2024-01-08 22:36:01 -0300

    feat: :test_tube:

--- src/components/organisms/Gallery/Slider/index.tsx
+++ src/components/organisms/Gallery/Slider/index.tsx
@@ -8,6 +8,9 @@ export default async function Slider() {
 	const data: any = await request({ query: queryGallery, revalidate: 30 });
 	const slides = data.allGallerySigas;
 
+
+
+	
 	return (
 		<div className='w-full mx-auto lg:h-[320px] 2xl:h-[400px] mt-5 h-[288px]'>
 			<Carousel
