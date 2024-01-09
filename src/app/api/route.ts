c8b09361f7e3a3a97ed54dd7873904446b70afd8
sirdav1d <ddavid.diniz@gmail.com>
2024-01-09 09:15:30 -0300

    feat: :sparkles: construindo api route

---
+++ src/app/api/route.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export default async function revalidateHandler(
+	req: NextRequest,
+	res: NextResponse,
+) {
+	return;
+}
